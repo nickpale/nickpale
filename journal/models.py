24d2f650f8ff3716bfeff6874b145528993d7cad
whiteisnick <whiteisnick@gmail.com>
2017-04-10 09:27:12 -0400

    Began journal model

--- journal/models.py
+++ journal/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class JournalEntry(models.Model):
+    heading_text = models.CharField(max_length=200)
+    entry_text = models.TextField()
+    pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.entry_text
