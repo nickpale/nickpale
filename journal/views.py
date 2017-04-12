65dfb8cea7805e815a96306e26746295535924da
whiteisnick <whiteisnick@gmail.com>
2017-04-11 22:16:17 -0400

    beginnings of templating

--- journal/views.py
+++ journal/views.py
@@ -1,9 +1,18 @@
+from django.http import Http404
 from django.shortcuts import render
-from django.http import HttpResponse
+
+from .models import JournalEntry
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the journal index.")
+    latest_entry_list = JournalEntry.objects.order_by('-pub_date')[:5]
+    context = {'latest_entry_list': latest_entry_list}
+    return render(request, 'journal/index.html', context)
 
 def entry(request, journal_entry_id):
-    return HttpResponse("You're looking at journal entry %s." % journal_entry_id)
+    try:
+        journal_entry = JournalEntry.objects.get(pk=journal_entry_id)
+    except JournalEntry.DoesNotExist:
+        raise Http404("Journal entry does not exist")
+    return render(request, 'journal/entry.html',
+                  {'journal_entry': journal_entry})
