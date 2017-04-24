42114212d9843faedd4ba9f485ff992546601eef
whiteisnick <whiteisnick@gmail.com>
2017-04-23 21:19:27 -0400

    added published_recently method and its tests

--- journal/models.py
+++ journal/models.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 class JournalEntry(models.Model):
     heading_text = models.CharField(max_length=200)
@@ -6,3 +9,7 @@ class JournalEntry(models.Model):
     pub_date = models.DateTimeField('date published')
     def __str__(self):
         return self.entry_text
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
--- journal/tests.py
+++ journal/tests.py
@@ -1,3 +1,32 @@
+import datetime
+
+from django.utils import timezone
 from django.test import TestCase
 
-# Create your tests here.
+from .models import JournalEntry
+
+class JournalEntryMethodTests(TestCase):
+
+    def test_was_published_recently_with_future_entries(self):
+        """
+        was_published_recently() should return False for entries whose pub_date is in the future.
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_entry = JournalEntry(pub_date=time)
+        self.assertIs(future_entry.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_entry(self):
+        """
+        was_published_recently() should return False for entries whose pub_date is older than 1 day.
+        """
+        time = timezone.now() - datetime.timedelta(days=30)
+        old_entry = JournalEntry(pub_date=time)
+        self.assertIs(old_entry.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_entry(self):
+        """
+        was_published_recently() should return True for entries whose pub_date is within the last day.
+        """
+        time = timezone.now() - datetime.timedelta(hours=1)
+        recent_entry = JournalEntry(pub_date=time)
+        self.assertIs(recent_entry.was_published_recently(), True)
