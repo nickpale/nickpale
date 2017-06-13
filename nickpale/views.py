2fc986838a0d56feb6dd0ea6fa64a0d1c9be3594
whiteisnick <whiteisnick@gmail.com>
2017-06-12 22:37:37 -0400

    trimming the fat and adding style to home

--- nickpale/views.py
+++ nickpale/views.py
@@ -8,20 +8,8 @@ class AboutPageView(TemplateView):
     template_name = "nickpale/about.html"
 
 
-class LoopPageView(TemplateView):
-    template_name = "nickpale/loops.html"
-
-
-class AlbumPageView(TemplateView):
-    template_name = "nickpale/albums.html"
-
-
 class DonatePageView(TemplateView):
     template_name = "nickpale/donate.html"
-
-
-class QuestionPageView(TemplateView):
-    template_name = "nickpale/question.html"
 
 
 class NewsPageView(TemplateView):
