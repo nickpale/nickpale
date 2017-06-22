2e864ee5ef938ac20c62d711d40cfb9aa12abd90
whiteisnick <whiteisnick@gmail.com>
2017-06-22 00:33:03 -0400

    deploy

--- nickpale/settings.py
+++ nickpale/settings.py
@@ -163,9 +163,11 @@ if os.environ['PROD']:
     # This is used by the `static` template tag from `static`, if you're using that. Or if anything else
     # refers directly to STATIC_URL. So it's safest to always set it.
     STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+    MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 
     # Tell the staticfiles app to use S3Boto storage when writing the collected static files (when
     # you run `collectstatic`).
     STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
     SECRET_KEY = os.environ['SECRET_KEY']
